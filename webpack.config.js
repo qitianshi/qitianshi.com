e6bb2702455719577c4b2c991f422d8f269008fa
qitianshi <65122416+qitianshi@users.noreply.github.com>
2023-06-04 16:44:50 +0800

    Set up webpack

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+// webpack config file.
+
+// Copyright 2023 Qi Tianshi. All rights reserved.
+
+
+const path = require("path");
+
+module.exports = {
+    mode: "none",       // Use `--mode` instead.
+    watch: false,       // Use `--watch` in development.
+    entry: path.join(__dirname, "src", "_webpack", "main.js"),
+    output: {
+        path: path.resolve(__dirname, "src", "assets", "scripts"),
+        filename: "bundle.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /.js$/,
+                exclude: [
+                    path.resolve(__dirname, "node_modules"),
+                    path.resolve(__dirname, "bower_components"),
+                ],
+                loader: "babel-loader",
+                // query: {
+                //     presets: ["env"],
+                // },
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".json", ".js", ".jsx"],
+    },
+};
