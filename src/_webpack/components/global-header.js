43a15c0859e0a4b50bf7750eb0fd3b05c212c164
qitianshi <65122416+qitianshi@users.noreply.github.com>
2023-06-14 23:30:05 +0800

    Add script for toggling navigation links on mobile

--- src/_webpack/components/global-header.js
+++ src/_webpack/components/global-header.js
@@ -3,9 +3,38 @@
 // Copyright 2023 Qi Tianshi. All rights reserved.
 
 
+/**
+ * Handles the event of the user clicking the hamburger button, by revealing
+ * hidden navigation links.
+ */
+function onUserClickHamburgerButton() {
+
+    // Reads the value of the hamburger button checkbox, and applies a class to
+    // the navigation links. CSS will cause the links to appear.
+
+    const navLinksClasses = document
+        .getElementById("global-header__navigation-links")
+        .classList;
+
+    const hamburgerIsChecked = document
+        .getElementById("global-header__hamburger-checkbox")
+        .checked;
+
+    const classToToggle = "global-header__navigation-links--mobile-visible";
+
+    if (hamburgerIsChecked) {
+        navLinksClasses.add(classToToggle);
+    } else {
+        navLinksClasses.remove(classToToggle);
+    }
+
+}
+
 const GlobalHeader = {
     init: function () {
-
+        document
+            .getElementById("global-header__hamburger-checkbox")
+            .addEventListener("change", onUserClickHamburgerButton);
     },
 }
 
