a22b84134cbe4e15bfbf65e3a59ef73b865aa373
qitianshi <65122416+qitianshi@users.noreply.github.com>
2023-10-01 17:26:44 +0800

    Restructure resizeTextarea function

--- src/_webpack/components/form.js
+++ src/_webpack/components/form.js
@@ -5,41 +5,38 @@
 
 /**
  * Dynamically changes the height of a textarea to always accommodate its
- * value. Each textarea is followed by a sizing element which is styled to
- * exactly match the text of the textarea; thus, by mirroring the text from the
- * textarea to the sizing element, the height of the textarea expands to fit
- * its contents.
+ * value.
  */
-function resizeTextarea() {
+function resizeTextarea(event) {
 
-    // Gets all textarea elements that are part of a .c-form__text-field.
-    const textareaNodes
-        = document.querySelectorAll(".c-form__text-field textarea");
+    // Each textarea is followed by a sizing element which is styled to exactly
+    // match the text of the textarea; thus, by mirroring the text from the
+    // textarea to the sizing element, the height of the textarea expands to
+    // fit its contents.
 
-    // Adds an event listener to each textarea to listen for changes to its
-    // value, and copies the value to the sizing element.
-    textareaNodes.forEach(function (textarea) {
-        textarea.addEventListener("input", function (event) {
+    const targetTextarea = event.target;
 
-            const targetTextarea = event.target;
-
-            targetTextarea
-                .parentElement
-                .querySelector(
-                    `pre[id='${targetTextarea.id}__textarea-sizing'] > span`
-                )
-                .textContent
-                = targetTextarea.value;
-
-        });
-    });
+    // Copies the value of the textarea to the sizing element.
+    targetTextarea
+        .parentElement
+        .querySelector(
+            `pre[id='${targetTextarea.id}__textarea-sizing'] > span`
+        )
+        .textContent
+        = targetTextarea.value;
 
 }
 
 const Form = {
     init: function () {
 
-        resizeTextarea();
+        // Adds an event listener to each textarea to resize it to fit its
+        // contents.
+        document
+            .querySelectorAll(".c-form__text-field textarea")
+            .forEach(function (textarea) {
+                textarea.addEventListener("input", resizeTextarea);
+            });
 
     }
 };
